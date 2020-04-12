5f099a5e5e8a6530b367c49bfef4eefccefa4563
sandiCzech <sanda.jakubhtc@gmail.com>
2020-04-12 17:33:09 +0200

    admin

---
+++ src/Admin/presenters/TirePresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Scrapyard module for webcms2.
+ * Copyright (c) @see LICENSE
+ */
+
+namespace AdminModule\ScrapyardModule;
+
+use Nette\Forms\Form;
+use WebCMS\ScrapyardModule\Entity\Tire;
+
+/**
+ *
+ * @author Jakub Sanda <jakub.sanda@webcook.cz>
+ */
+class TirePresenter extends BasePresenter
+{
+    
+    private $car;
+
+    protected function startup()
+    {
+    	parent::startup();
+    }
+
+    protected function beforeRender()
+    {
+	   parent::beforeRender();
+    }
+
+    public function actionDefault($idPage)
+    {
+    }
+
+    public function renderDefault($idPage){
+        $this->reloadContent();
+        
+        $this->template->idPage = $idPage;
+    }
+
+    
+}
